9f9e310ff24cf5d21c5b038bfa0a01ea9724bb55
wangzd <734327367@qq.com>
2019-03-08 17:42:19 +0800

    添加js hover

--- RESOURCES/js/index.js
+++ RESOURCES/js/index.js
@@ -1,6 +1,13 @@
-$('.view0').hover(function () {
-    $('.view0').css('backgroundColor', '#bbb');
-},function () {
-    $('.view0').css('backgroundColor', 'none');
-}
-)
+$('.top-menu ul li').hover(function () {
+    $(this).css("color", '#6c1');
+}, function () {
+    $(this).css("color", '#fff');
+})
+
+$('.view ul li').hover(function () {
+    $(this).css('backgroundColor','#1B1B1B');
+    $(this).css('color','#6c1');
+}, function () {
+    $(this).css('backgroundColor','transparent');
+    $(this).css('color','#fff');
+})
