bcd9dd4d6bdca48da4a94ad5ce5b21edb9520b8f
wangzd <734327367@qq.com>
2019-03-13 09:55:46 +0800

    eee

--- RESOURCES/js/index.js
+++ RESOURCES/js/index.js
@@ -5,17 +5,23 @@ $('.top-menu ul li').hover(function () {
 })
 
 $('.view ul li').hover(function () {
-    $(this).css('backgroundColor','#1B1B1B');
-    $(this).css('color','#6c1');
+    $(this).css('backgroundColor', '#1B1B1B');
+    $(this).css('color', '#6c1');
 }, function () {
-    $(this).css('backgroundColor','transparent');
-    $(this).css('color','#fff');
+    $(this).css('backgroundColor', 'transparent');
+    $(this).css('color', '#fff');
 })
 
 $('.details').hover(function () {
-    $(this).css('backgroundColor','#1B1B1B');
-    $(this).css('color','#6c1');
+    $(this).css('backgroundColor', '#1B1B1B');
+    $(this).css('color', '#6c1');
 }, function () {
-    $(this).css('backgroundColor','transparent');
-    $(this).css('color','#1B1B1B');
+    $(this).css('backgroundColor', 'transparent');
+    $(this).css('color', '#1B1B1B');
 })
+
+// $('.active').hover(function () {
+//     $(this).css('backgroundImage')
+// }, function () {
+
+// })
